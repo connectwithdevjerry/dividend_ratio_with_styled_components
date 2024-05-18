e1ac5dbeb60b53b9aae43cadded3a9c9973e8f59
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-05-19 00:12:42 +0100

    just commit - dividend ratio

--- src/components/MainYield.jsx
+++ src/components/MainYield.jsx
@@ -1,21 +1,19 @@
-// import React from 'react'
-// import CustomizedTables from './CustomTable'
-// import { useEffect } from 'react';
+import React from 'react'
+import CustomizedTables from './CustomTable'
+import { useEffect } from 'react';
 
-// export const MainYield = () => {
+export const MainYield = () => {
 
-//   // const url = "https://mews-systems.gitbook.io/connector-api/operations/rates"
+  // const url = "https://mews-systems.gitbook.io/connector-api/operations/rates"
 
-//   // useEffect(() => {
-//   //   const opera_data = axios(url)
-//   //   console.log(opera_data)
-//   // })
+  // useEffect(() => {
+  // })
 
-//   return (
-//     <>
-//         <CustomizedTables title="Final Advised Rates" />
-//         <CustomizedTables title="Overall Pace" />
-//         <CustomizedTables title="Today vs YearAgo" />
-//     </>
-//   )
-// }
+  return (
+    <>
+        <CustomizedTables title="Final Advised Rates" />
+        <CustomizedTables title="Overall Pace" />
+        <CustomizedTables title="Today vs YearAgo" />
+    </>
+  )
+}
